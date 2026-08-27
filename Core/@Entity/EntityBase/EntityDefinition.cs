f87c0306992258a6b680c8779dc8a064e524a6d2
PreThink <ilia-samarin@bk.ru>
2026-08-27 19:21:57 +0700

    Удаляем лишнее

--- Core/@Entity/EntityBase/EntityDefinition.cs
+++ Core/@Entity/EntityBase/EntityDefinition.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public abstract class EntityDefinition<TDefinition> : EntityBase, IEntityDefinitionReceiver 
+public abstract class EntityDefinition<TDefinition> : EntityBase 
     where TDefinition : IEntityInfo
 {
     [field: SerializeField, Header("Definition")] public TDefinition Definition { get; private set; }
@@ -9,20 +9,5 @@ public abstract class EntityDefinition<TDefinition> : EntityBase, IEntityDefinit
     protected override void InitializeEntityInfo()
     {
         Info = new EntityInfoContainer(Definition);
-    }
-
-    /// <inheritdoc />
-    /// <remarks>
-    /// Пересобирает <c>Info</c>: он строится в <c>Awake</c> по тому определению, что лежит
-    /// в префабе, и без пересборки сущность осталась бы с прежним именем и иконкой.
-    /// </remarks>
-    public bool TryAssignDefinition(IEntityInfo definition)
-    {
-        if (definition is not TDefinition typed)
-            return false;
-
-        Definition = typed;
-        InitializeEntityInfo();
-        return true;
     }
 }
